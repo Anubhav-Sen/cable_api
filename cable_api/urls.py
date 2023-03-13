12983b9422654fd4d90810948ebbfee74ecd2f1b
Anubhav Sen <anubhav.sen@code.berlin>
2023-03-13 13:45:19 +0100

    Created a test api endpoint to check if rest framework is working

--- cable_api/urls.py
+++ cable_api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('test', views.test, name='test')
+]
--- cable_api/views.py
+++ cable_api/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def test(request):
+    return Response('test', status=status.HTTP_200_OK)    
