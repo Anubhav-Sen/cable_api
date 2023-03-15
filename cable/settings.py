d67d3a7c810ac48b3c6a25792f425c0ee4cb8b51
Anubhav Sen <anubhav.sen@code.berlin>
2023-03-15 13:24:45 +0100

    Installed simple jwt and added token and token/refresh endpoints to the api

--- cable/settings.py
+++ cable/settings.py
@@ -41,6 +41,12 @@ INSTALLED_APPS = [
     'cable_api.apps.CableApiConfig',
 ]
 
+REST_FRAMEWORK = {   
+    'DEFAULT_AUTHENTICATION_CLASSES': (   
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- cable_api/urls.py
+++ cable_api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('test', views.test, name='test')
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('test/', views.test, name='test')
 ]
