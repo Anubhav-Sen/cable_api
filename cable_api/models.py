e4ddf2193267bad4481d45051ed821378907f6f0
Anubhav Sen <anubhav.sen@code.berlin>
2023-03-16 15:09:20 +0100

    Created profile image path function and added a __str__ method to the user model.

--- cable_api/models.py
+++ cable_api/models.py
@@ -1,16 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
+def profile_image_path(instance, filename):
+    """
+    A function responsible for generating a file path to store the users profile image.
+    """
+    path = f'users/{instance.email_address}/profile_image/{filename}'
+
+    return path
+
 class UserManager(BaseUserManager):
     """
     A Class that defines methodes to manage the custom user model.
     """
-
     def create_user(self, user_name, email_address, password):
         """
         A method responsible for creating a new user and saving them to the database.
         """
-
         user = self.model(
             email_address = self.normalize_email(email_address),
             user_name = user_name,
@@ -26,7 +32,6 @@ class UserManager(BaseUserManager):
         """
         A method responsible for creating a new user with administrative privilages set to true. 
         """
-
         user = self.model(
             email_address = self.normalize_email(email_address),
             user_name = user_name,
@@ -49,13 +54,13 @@ class User(AbstractBaseUser):
     - The manager for the custom user model.
     - The username field.
     - The required fields.
+    - The __str__ method for the user model.
     - Methods to handel permissions.
     """
-
     user_name = models.CharField(max_length=50)
     email_address = models.EmailField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
-    profile_image = models.ImageField(upload_to='', null=True, blank=True)
+    profile_image = models.ImageField(upload_to=profile_image_path, null=True, blank=True)
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
@@ -66,6 +71,12 @@ class User(AbstractBaseUser):
 
     USERNAME_FIELD = 'email_address'
     REQUIRED_FIELDS = ['user_name', 'password']
+
+    def __str__(self):
+        """
+        Method that returns the email address of the user to represent the user object.
+        """
+        return self.email_address
 
     def has_perm(self, perm, obj=None):
         """
