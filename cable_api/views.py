9f76e609cd967b9f32c98b950a180d0bd76f3fcb
Anubhav Sen <anubhav.sen@code.berlin>
2023-03-21 14:24:51 +0100

    Upated the users enpoint to return a list of users.

--- cable_api/views.py
+++ cable_api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from cable_api.serializers import UserSerializer
 
 @api_view(['GET'])
 def users(request):
@@ -8,8 +10,13 @@ def users(request):
     A function that defines the "api/users/" endpoint.
     """
 
-    
-    return Response('users', status=status.HTTP_200_OK)
+    users = get_user_model().objects.all()
+
+    users_serializer = UserSerializer(users, many=True)
+
+    response_dict = {'users': users_serializer.data}
+
+    return Response(response_dict, status=status.HTTP_200_OK)
 
 @api_view(['GET'])
 def test(request):
