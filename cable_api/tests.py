1b883521c8f262ea2d96253325952237b2db7c12
Anubhav Sen <anubhav.sen@code.berlin>
2023-03-22 17:00:44 +0100

    Created a basic unit test class for the users endpoint.

--- cable_api/tests.py
+++ cable_api/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class TestUsers(APITestCase):
+    """
+    A class to test the "api/users/" endpoint.
+    """
+    def setUp(self):
+        pass
+    
+    def test_users_GET(self):
+        """
+        A method to test the GET method of the "api/users/" endpoint.
+        """
+        endpoint = reverse('users')
+
+        response = self.client.get(endpoint)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
