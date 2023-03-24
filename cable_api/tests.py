b8e69007832dd76b61156f88d5ea09cbf9c70007
Anubhav Sen <anubhav.sen@code.berlin>
2023-03-24 17:13:30 +0100

    Fixed issue where factory sequence wouldn't reset by adding a teardown method to the user endpoint test.

--- cable_api/tests.py
+++ cable_api/tests.py
@@ -2,6 +2,7 @@ import json
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
 from cable_api.factory import UserFactory
 
 class TestUsers(APITestCase):
@@ -9,17 +10,18 @@ class TestUsers(APITestCase):
     A class to test the "api/users/" endpoint.
     """
     def setUp(self):
-        
-        self.user_factory = UserFactory.create_batch(5)
+        """
+        A method to define the base setup for this test class.
+        """
+        self.user_factory = UserFactory    
+        self.user_factory.create_batch(5)
         self.maxDiff = None
-    
+
     def test_users_GET(self):
         """
         A method to test the GET method of the "api/users/" endpoint.
         """
         endpoint = reverse('users')
-
-        response = self.client.get(endpoint)
 
         users_list = []
 
@@ -36,6 +38,30 @@ class TestUsers(APITestCase):
 
         expected_response = {'users': users_list}
 
+        response = self.client.get(endpoint)
+
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(expected_response, json.loads(response.content))
-        
+
+    def test_users_POST(self):
+        """
+        A method to test the POST method of the "api/users/" endpoint.
+        """
+        endpoint = reverse('users')
+
+        request_dict = {
+            
+        }
+
+        users = get_user_model().objects.all()
+        print(users)
+
+        response = self.client.post(endpoint)
+
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+
+    def tearDown(self):
+        """
+        A method to teardown the data generated using the setup method.
+        """
+        self.user_factory.reset_sequence()
