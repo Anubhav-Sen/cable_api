2564411c6cc750a6b7adbb1e7614ac1959ada797
Anubhav Sen <anubhav.sen@code.berlin>
2023-04-28 12:00:14 +0200

    Set debug to false.

--- cable/settings.py
+++ cable/settings.py
@@ -30,7 +30,7 @@ MEDIA_URL = 'media/'
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
